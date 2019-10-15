7307c0d57d70a12fb14a1b4ab7d847ba366ea717
Ellyo Freitas <ellyofreitas13@gmail.com>
2019-10-15 01:11:31 -0300

    second commit :)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import Reducer from './hooks/Reducer';
 import State from './hooks/State';
 import Effect from './hooks/Effect';
 import Memo from './hooks/Memo';
-import Context from './hooks/Context';
 
 function App() {
-  return (
-    <div className="App">
-      <Context />
-    </div>
-  );
+  return <div className="App">{/* put one hook to show a example */}</div>;
 }
 
 export default App;
